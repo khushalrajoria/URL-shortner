4cfac1a68a22cfa6deeb0f5b1b15d52d5609569e
Khushal rajoria <rajoriakhushal@gmail.com>
2024-07-18 16:21:37 +0530

    used shortid instead of nanoid

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,17 @@
+const {shortid}=require("shortid");
+const URL= require("../models/url")
 async function handleGenerateNewShortURL(req,res){
-    
+    const body=req.body;
+    if(!body.url) {return res.status(400).json({error :"url not given"})}
+    const shortID =shortid();
+    await URL.create({
+        shortId:shortID,
+        redirectUrl:body.url,
+        visitHistory:[],
+    });
+    return res.json({id:shortID});
+}
+
+module.exports={
+    handleGenerateNewShortURL,
 }
--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const app=express();
-
+const urlRoute=require("./routes/user");
+const {connectMongoDb}=require("./connect");
 const PORT =8001;
+
+connectMongoDb("mongodb://127.0.0.1:27017/short-URL").then(()=>console.log("MongoDB connected"))
+
+app.use("/url",urlRoute);
+
 app.listen(PORT,()=> console.log(`Server Started at port ${PORT}`));
